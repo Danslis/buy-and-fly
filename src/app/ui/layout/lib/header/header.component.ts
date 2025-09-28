696bb7b515c9c86ddd1db8c289a9e9c125d6910f
Dart <Danslis@yandex.ru>
2025-09-28 12:03:43 +0300

    [ui][layout] Adding header styles

--- src/app/ui/layout/lib/header/header.component.ts
+++ src/app/ui/layout/lib/header/header.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { ContainerComponent } from '@baf/ui/container';
+import { LogoComponent } from '@baf/ui/icons';
 
 @Component({
   selector: 'baf-header',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, ContainerComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
