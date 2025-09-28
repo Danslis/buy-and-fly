9346d6fa0bb4bb599c3078cc355a35c1cd0a0fb1
Dart <Danslis@yandex.ru>
2025-09-28 12:19:32 +0300

    [core] Adding core lib

---
+++ src/app/core/lib/coercoin/coercoin.ts
@@ -0,0 +1,8 @@
+/**
+ * @see https://github.com/angular/components/blob/main/src/cdk/coercion/boolean-property.ts
+*/
+
+
+export function coerceBooleanProperty(value: unknown): boolean {
+  return value != null && `${value}` !== 'false';
+}
