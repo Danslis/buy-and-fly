8767901af95d4a9ab89d7d6c3a7b0d5d4263c833
Dart <Danslis@yandex.ru>
2025-10-04 18:21:13 +0300

    [search] Adding pages

---
+++ src/app/search/ui/form/lib/search-form.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'baf-search-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-form.component.html',
+  styleUrl: './search-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class SearchFormComponent { }
