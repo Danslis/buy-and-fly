64e6e3d90bb4a7c3777b4a8d7d637cf289c9ef1c
Dart <Danslis@yandex.ru>
2025-10-06 00:33:26 +0300

    [search][form] Adding base structure

---
+++ src/app/search/ui/fields/index.ts
@@ -0,0 +1,5 @@
+export * from "./lib/search-date/search-date.component";
+export * from "./lib/search-destination/search-destination.component";
+export * from "./lib/search-group/search-group.component";
+export * from "./lib/search-passengers/search-passengers.component"
+export * from "./lib/search-reverse/search-reverse.component"
--- src/app/search/ui/form/lib/search-form.component.ts
+++ src/app/search/ui/form/lib/search-form.component.ts
@@ -1,17 +1,54 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+import { FormFor } from '@baf/core';
+import { SearchForm } from '@baf/search/common';
+
+import { SearchDateComponent } from '../../fields/lib/search-date/search-date.component';
+import { SearchDestinationComponent } from '../../fields/lib/search-destination/search-destination.component';
+import { SearchGroupComponent } from '../../fields/lib/search-group/search-group.component';
+import { SearchPassengersComponent } from '../../fields/lib/search-passengers/search-passengers.component';
+import { SearchReverseComponent } from '../../fields/lib/search-reverse/search-reverse.component';
+
+const initialForm = new FormGroup<FormFor<SearchForm>>({
+  from: new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  }),
+  to: new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  }),
+  startDate: new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  }),
+  endDate: new FormControl<string>('', {
+    nonNullable: true,
+    validators: [],
+  }),
+  passengers: new FormControl<number>(1, {
+    nonNullable: true,
+    validators: [Validators.required],
+  }),
+});
+
 @Component({
   selector: 'baf-search-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    SearchGroupComponent,
+    SearchDestinationComponent,
+    SearchReverseComponent,
+    SearchDateComponent,
+    SearchPassengersComponent,
+  ],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class SearchFormComponent {
-  readonly form = new FormGroup({
-    from: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
-  });
+  readonly form = initialForm;
 }
