d3180da6955c83ea3f97a072f4be9350b14169ef
Dart <Danslis@yandex.ru>
2025-10-14 23:46:54 +0300

    [core] Adding envs

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/ssr';
 
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { provideEnv } from './core/lib/env/environment';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+  providers: [provideServerRendering(), provideEnv()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import * as dotenv from 'dotenv';
 
 import { App } from './app/app';
 import { config } from './app/app.config.server';
+
+dotenv.config();
 
 const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
 
