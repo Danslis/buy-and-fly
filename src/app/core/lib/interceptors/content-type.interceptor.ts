7d51c8dcfa052bd265cbb1a68c1749a3e1ee5b4a
Dart <Danslis@yandex.ru>
2025-10-26 19:48:13 +0300

    [core] Removing unused code

--- src/app/core/lib/interceptors/content-type.interceptor.ts
+++ src/app/core/lib/interceptors/content-type.interceptor.ts
@@ -1,8 +1,9 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import type { HttpInterceptorFn } from '@angular/common/http';
 
 export const contentTypeInterceptor: HttpInterceptorFn = (req, next) => {
   if (!req.headers.has('Content-Type') && req.headers.get('enctype') !== 'multipart/form-data') {
     req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
   }
+
   return next(req);
 };
--- src/app/core/lib/interceptors/visitor.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-import { getVisitorUuid } from '../utils/visitor';
-
-export const visitorInterceptor: HttpInterceptorFn = (req, next) => {
-  const visitorUuid = getVisitorUuid();
-  req = req.clone({ headers: req.headers.set('Visitor-uuid', visitorUuid) });
-  return next(req);
-};
