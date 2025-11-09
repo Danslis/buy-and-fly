49e01b36b874586ea0606149db0a1eee124dfc52
Dart <Danslis@yandex.ru>
2025-11-09 19:07:25 +0300

    [ui] Change structure

---
+++ src/app/ui/title/lib/title.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+@Component({
+  selector: 'baf-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './title.component.html',
+  styleUrl: './title.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TitleComponent { }
