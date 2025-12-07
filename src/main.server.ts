691f23f5434d67b4686850ea5327aa4cfc722993
Dart <Danslis@yandex.ru>
2025-12-07 21:39:45 +0300

    [i18n] Adding localization

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,12 +1,9 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
-import * as dotenv from 'dotenv';
 
 import { App } from './app/app';
 import { config } from './app/app.config.server';
-
-dotenv.config();
 
 const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
 
