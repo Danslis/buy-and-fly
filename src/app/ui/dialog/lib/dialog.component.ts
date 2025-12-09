384f788658aff3d98d546c4f7e0d5af33c464c41
Dart <Danslis@yandex.ru>
2025-12-09 22:44:00 +0300

    [home] Start traveling

---
+++ src/app/ui/dialog/lib/dialog.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'baf-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DialogComponent {
+}
