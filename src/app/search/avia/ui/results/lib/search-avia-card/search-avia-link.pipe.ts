e47364e90ebfb49459f094a74899ab7fe05106ca
Dart <Danslis@yandex.ru>
2025-12-21 23:01:29 +0300

    [search] Adding third part logic for search

---
+++ src/app/search/avia/ui/results/lib/search-avia-card/search-avia-link.pipe.ts
@@ -0,0 +1,15 @@
+import { inject, Pipe, PipeTransform } from '@angular/core';
+
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'searchAviaLink',
+  standalone: true,
+})
+export class SearchAviaLinkPipe implements PipeTransform {
+  private domSanitizer = inject(DomSanitizer);
+
+  transform(link: string): SafeUrl {
+    return this.domSanitizer.bypassSecurityTrustUrl(`https://www.aviasales.ru${link}`);
+  }
+}
