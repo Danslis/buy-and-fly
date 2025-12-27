9b613aa0593e5223a6dcfb8056c33a915eddb362
Dart <Danslis@yandex.ru>
2025-12-27 21:17:19 +0300

    [search] Adding hotels photos

--- src/app/app.config.browser.ts
+++ src/app/app.config.browser.ts
@@ -1,10 +1,13 @@
+import 'hammerjs';
+
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+
+import { provideHammer } from '@baf/core';
 
 import { appConfig } from './app.config';
 
 const browserConfig: ApplicationConfig = {
-  providers: [
-  ],
+  providers: [provideHammer()],
 };
 
 export const config = mergeApplicationConfig(appConfig, browserConfig);
